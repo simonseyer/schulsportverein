1c5dca74e0e4fdb547c2f14183804d74a22dd824
Simon Seyer <simon.seyer@gmail.com>
2016-12-12 10:33:16 +0200

    Added image optimise step to build.sh

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ hugo
 sips --resampleHeight 800 docs/header/*
 sips --resampleHeight 400 docs/img/logo.png
 sips --resampleHeight 140 docs/img/partner/*
+
+# Optimize images
+/Applications/ImageOptim.app/Contents/MacOS/ImageOptim docs
